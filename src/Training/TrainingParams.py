f11a6d2234c9259f1ce41b6eb430816cebc1864a
Ristafan <martin.faehnrich@gmail.com>
2025-06-15 13:55:29 +0200

    Feat: Updated Training Params

--- src/Training/TrainingParams.py
+++ src/Training/TrainingParams.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from src.DataSetSplit.TrainingClasses import bat_species
 
-
 # Preprocessing Progress
 SPLITS_ALREADY_COMPUTED = False
 SPECTROGRAMS_ALREADY_COMPUTED = False
@@ -18,10 +17,11 @@ TEST_RATIO = 0.2                            # Validation ratio will be automatic
 SEED = 42
 
 # Preprocessing Parameters
+SAMPLE_RATE = 192000
 HIGHPASS_CUTOFF_FREQ = 16000
 N_FFT = 4096
-HOP_LENGTH = None                            # Default will be set to n_fft // 4
 WIN_LENGTH = 2048
+HOP_LENGTH = WIN_LENGTH // 2
 DENOISE_OPTION = "mean_subtraction"           # Options: 'mean_subtraction', 'median_filter'
 
 # Training Model Parameters
@@ -32,6 +32,11 @@ NUM_EPOCHS = 2
 LEARNING_RATE = 0.001
 DROPOUT_RATE = 0.3
 BATCH_NORM = False                          # Set to True if you want to use Batch Normalization in the model
+
+# Windows Parameters
+WINDOW_SIZE = 1.0
+OVERLAP_SIZE = 0.2
+LOSS_FILTER_THRESHOLD_PERCENTAGE = 0.8  # Percentage of the maximum loss to filter outliers
 
 # WandB Parameters
 USE_WANDB = True
