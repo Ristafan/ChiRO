4864cbcc7424217cab3c3978e31fcbfd6e87fd5e
Ristafan <martin.faehnrich@gmail.com>
2025-06-15 15:58:04 +0200

    Feat: General Train file

---
+++ src/Training/Train.py
@@ -0,0 +1,9 @@
+from TrainAlpha import main as train_alpha_main
+from TrainAlphaAttention import main as train_alpha_attention_main
+from TrainBeta import main as train_beta_main
+from TrainAlphaBeta import main as train_alpha_beta_main
+from TrainAlphaMIL import main as train_alpha_mil_main
+
+
+if __name__ == "__main__":
+    train_alpha_mil_main()
